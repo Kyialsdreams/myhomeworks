a38535677892c689bcd395fa285d5255496a302e
Kyialsdreams <101570325+Kyialsdreams@users.noreply.github.com>
2022-03-15 17:17:29 +0600

    I didn't have enough time to give you more examples
    void main() {
      int num=10;
      int num1=20;
      int num3=45;
      int num4=70;
      int num5=200;
      print (num1+num5);
      print (num5/num);
      print (num4-num3);
     
      String msg="Let's start learn Flutter!";
      String color="red";
      String $sum='12345';
      String course='Flutter';
      String my_words="Let's try!";
      print(msg);
      print(color);
      print($sum);
      print(course);
      print(my_words);
    
      dynamic name="Kyial";
      print(name);
      name="Aizhamal";
      print(name);
      name=12345;
      print(name);
      name='12345';
      print(name);
      name='Dart is for Flutter!';
      print(name);
    
      List array=[1, 5.6, 'laptops', "You can do it!"];
      print(array);
      List <int> number=[100, 200, 300];
      print(number);
      List <String> flutter1classmates=['Kyial', 'Akylai', 'Kanagat', 'Beka'];
      print(flutter1classmates);
      List <double> array_of_double=[1.0, 2.5, 50.80, 90.100];
      print(array_of_double);
      List trade$marks=['Apple', 'Samsung', 'LG'];
      print(trade$marks);
    
      Map Dartist={ 'name': 'Aidar', 'age':13, 'hobby': '3d modelling and coding'};
      print(Dartist);
    
      var details_of_classmates={'Username': 'Kanagat', 'password': #$1235, 'role': 'Student'};
      print(details_of_classmates);
    
      final my_variable=("Today is your opportunity to build the tomorrow you want!");
      print(my_variable);
    
      const my_motivation1= "Don't stop until you are proud";
      print(my_motivation1);
    
    }

---
+++ first_homework.dart
@@ -0,0 +1,55 @@
+void main() {
+  int num=10;
+  int num1=20;
+  int num3=45;
+  int num4=70;
+  int num5=200;
+  print (num1+num5);
+  print (num5/num);
+  print (num4-num3);
+  String msg="Let's start learn Flutter!";
+  String color="red";
+  String $sum='12345';
+  String course='Flutter';
+  String my_words="Let's try!";
+  print(msg);
+  print(color);
+  print($sum);
+  print(course);
+  print(my_words);
+
+  dynamic name="Kyial";
+  print(name);
+  name="Aizhamal";
+  print(name);
+  name=12345;
+  print(name);
+  name='12345';
+  print(name);
+  name='Dart is for Flutter!';
+  print(name);
+
+  List array=[1, 5.6, 'laptops', "You can do it!"];
+  print(array);
+  List <int> number=[100, 200, 300];
+  print(number);
+  List <String> flutter1classmates=['Kyial', 'Akylai', 'Kanagat', 'Beka'];
+  print(flutter1classmates);
+  List <double> array_of_double=[1.0, 2.5, 50.80, 90.100];
+  print(array_of_double);
+  List trade$marks=['Apple', 'Samsung', 'LG'];
+  print(trade$marks);
+
+  Map Dartist={ 'name': 'Aidar', 'age':13, 'hobby': '3d modelling and coding'};
+  print(Dartist);
+
+  var details_of_classmates={'Username': 'Kanagat', 'password': #$1235, 'role': 'Student'};
+  print(details_of_classmates);
+
+  final my_variable=("Today is your opportunity to build the tomorrow you want!");
+  print(my_variable);
+
+  const my_motivation1= "Don't stop until you are proud";
+  print(my_motivation1);
+
+}
